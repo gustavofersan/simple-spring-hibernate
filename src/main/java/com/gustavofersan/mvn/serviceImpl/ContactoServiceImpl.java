7965f6bd0ae837ae6bd02b1d8c2a59d97d7f542a
GustavoFernandez <gustavo.27.92@hotmail.com>
2014-05-04 00:19:15 -0500

    new changes

--- src/main/java/com/gustavofersan/mvn/serviceImpl/ContactoServiceImpl.java
+++ src/main/java/com/gustavofersan/mvn/serviceImpl/ContactoServiceImpl.java
@@ -3,30 +3,35 @@ package com.gustavofersan.mvn.serviceImpl;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gustavofersan.mvn.dao.ContactoDao;
 import com.gustavofersan.mvn.domain.Contacto;
 import com.gustavofersan.mvn.service.ContactoService;
 
+@Service
 public class ContactoServiceImpl implements ContactoService {
 
 	@Autowired
 	private ContactoDao contactoDao;
-	
+
+	@Transactional
 	@Override
 	public void guardarContacto(Contacto contacto) {
-		// TODO Auto-generated method stub
+		contactoDao.guardarContacto(contacto);
 	}
 
+	@Transactional
 	@Override
 	public ArrayList<Contacto> contactos() {
-		// TODO Auto-generated method stub
-		return null;
+		return contactoDao.contactos();
 	}
 
+	@Transactional
 	@Override
 	public void eliminarContacto(Long id) {
-		// TODO Auto-generated method stub
+		contactoDao.eliminarContacto(id);
 	}
 
 }
